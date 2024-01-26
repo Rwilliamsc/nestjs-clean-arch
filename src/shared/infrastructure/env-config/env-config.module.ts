07283576720ddd5f736bb8429f3b2e7cdaf9e4a1
Raphael Costa <rwilliam.sc@gmail.com>
2024-01-25 22:30:46 -0300

    Implement env config interface

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common'
 import { EnvConfigService } from './env-config.service'
+import { EnvConfig } from './env-config.interface'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
   providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule implements EnvConfig {
+  constructor(private readonly configService: ConfigService) {}
+  getAppPort(): number {
+    return Number(this.configService.get<number>('APP_PORT'))
+  }
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV')
+  }
+}
